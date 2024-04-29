6479da31b5fadac736e33993f9e755ef101d2e36
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-04-30 02:43:57 +0300

    Header_Cover

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Cover from './components/Cover'
+import Navbar from './components/Navbar'
+
+function App() {
+ 
+
+  return (
+    <div className=' '> 
+       
+     <header className=' px-2 sm:px-20 mx-auto bg-green-800'>
+        <Navbar></Navbar>
+<hr />
+        <Cover></Cover>
+     </header>
+
+
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+
+
+
+// import Reat from 'react';
+
+const Navbar = () => {
+    return (
+        <div className=" flex  h-24 justify-between items-center">
+            <section className=" flex justify-center items-center gap-2">
+                <img className=" border-4 p-2 border-teal-400 h-16 w-16  rounded-full" src="https://www.pngall.com/wp-content/uploads/2018/04/School.png" alt="" />  
+                <h3 className=" font-semibold text-white">
+                    Learn <br /> 
+                    With Sumith
+                </h3>
+            </section>
+
+            <section>
+                <button className=' bg-gray-800 hover:bg-gray-700 outline-none  py-2 px-4 text-white rounded-2xl'>Get Admission</button>
+            </section>
+        </div>
+    );
+};
+
+export default Navbar;
